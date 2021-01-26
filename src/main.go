35df1149240c9d0b0bbdb776cac18df981a3d599
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2021-01-26 20:25:58 -0300

    finalizado logica e tests de soma

---
+++ src/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func soma(a, b int) int {
+  return a + b
+}
+
+func main() {
+
+  res := soma(5, 5)
+  fmt.Println("Resultado da soma de 5+5=", res)
+
+}
---
+++ src/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestSoma_4(t *testing.T){
+  resultado := soma(2,2)
+  if resultado != 4{
+    t.Errorf("Soma esperada: %d, obtida: %d", 4, resultado)
+  }
+}
+
+func TestSoma_5(t *testing.T){
+  resultado := soma(2,-3)
+  if resultado != -1{
+    t.Errorf("Soma esperada: %d, obtida: %d", 4, resultado)
+  }
+}
+
+func TestSoma_10(t *testing.T){
+  resultado := soma(-2,12)
+  if resultado != 10{
+    t.Errorf("Soma esperada: %d, obtida: %d", 4, resultado)
+  }
+}
